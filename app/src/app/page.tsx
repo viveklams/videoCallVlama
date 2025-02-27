18dfb596e4bfb4b758356560d98f75a7201e15bb
viveklams <viveklamgadey@gmail.com>
2025-02-28 00:40:19 +0530

    Now we are gonna install database :-> we are using convex for database : for real time updates and is a modren serverless database

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Home</h1>
+   <div className="m-10">
+     homePage
    </div>
   );
 }
